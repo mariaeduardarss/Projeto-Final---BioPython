1a1131a1bf6ee172b94a9cd6c0483a7c8b7c3046
Maria Eduarda <mariaeduardarsouza@outlook.com>
2025-07-19 22:05:17 -0300

    organismo_fasta.py

--- bio/organismo_fasta.py
+++ bio/organismo_fasta.py
@@ -1,16 +1,13 @@
 from bio.sequencia import Sequencia
 
 class OrganismoFasta:
-    def __init__(self, id, nome, sequencia):
+       def __init__(self, id, nome, sequencia):
         self.id = id
         self.nome = nome
         self.sequencia = Sequencia(sequencia)
-    
+
     def __str__(self):
         return f"{self.id} | {self.nome}"
-        
+
     def calcular_percentual(self, bases):
         return self.sequencia.calcular_percentual(bases)
-
-    def traduzir_proteina(self):
-        return self.sequencia.traduzir()
